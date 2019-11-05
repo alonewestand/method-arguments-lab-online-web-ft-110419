f700e73cf9ea58f8be4453b76df758025a6a3ddc
Ken Goad <goadke@g.cofc.edu>
2019-11-05 03:34:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
-def introduction(name, language)
-  "Hi, my name is" #{name} 
-  
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}." 
+end
+
